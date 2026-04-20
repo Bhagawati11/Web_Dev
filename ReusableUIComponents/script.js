d62c8c05e17b81c24a1b864e642aebdd264ded9d
Bhagawati <kadambhagawati11@gmail.com>
2026-04-20 22:57:41 +0530

    ui components

---
+++ ReusableUIComponents/script.js
@@ -0,0 +1,17 @@
+// script.js
+
+function openModal() {
+    document.getElementById('modal').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+}
+
+// Close modal when clicking outside
+window.onclick = function(event) {
+    const modal = document.getElementById('modal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+}
